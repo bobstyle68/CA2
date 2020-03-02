d24bd118afe69512227fea79bca9b37d6e71a8c9
bobstyle68 <60360117+bobstyle68@users.noreply.github.com>
2020-03-02 09:19:44 +0000

    Initial commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Speed = 2;
+
+    private void Start()
+    {
+        Invoke("DestroyBullet", 2);
+    }
+    //sets the velocity of the bullet
+    public void SetDirection(Vector2 direction)
+    {
+        GetComponent<Rigidbody2D>().velocity = direction * Speed;
+    }
+
+    //can be extended by later (virtual)
+    public virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        DestroyBullet();
+    }
+
+    //can be extended by later (virtual)
+    public virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBullet();
+    }
+
+    void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Weapon activeWeapon;
+    public Transform AttachPoint;
+
+    void Start()
+    {
+    }
+
+    private void PickUpWeapon()
+    {
+        activeWeapon = Instantiate(activeWeapon, transform);
+        activeWeapon.transform.position = AttachPoint.position;
+    }
+
+    void DropWeapon()
+    {
+
+    }
+
+    void Update()
+    {
+        //if activeWeapon is not null (has teh weapon prefab been cloned)
+        if(activeWeapon)
+        {
+            if(activeWeapon.IsAutomatic)
+            {
+                if (Input.GetButton("Fire1"))//left mouse button and right controller trigger
+                {
+                    if (activeWeapon.HasAmmo())
+                        activeWeapon.Fire(activeWeapon.Spawn.position);
+                }
+            }
+            else
+            {
+                if(Input.GetButtonDown("Fire1"))//left mouse button and right controller trigger
+                {
+                    if (activeWeapon.HasAmmo())
+                        activeWeapon.Fire(activeWeapon.Spawn.position);
+                }
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Weapon"))
+        {
+            activeWeapon = collision.GetComponent<Weapon>();
+            PickUpWeapon();
+
+            Destroy(collision.gameObject);
+        }
+    }
+}
